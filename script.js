87bbd1e2288463a6a182f8f083db5776133b97cc
tharshanaprabhu <tharshanaprabhu@gmail.com>
2026-01-01 16:57:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-let lightOn = false;
-let fanOn = false;
-let securityLocked = true;
-let temperature = 25;
+document.addEventListener("DOMContentLoaded", function () {
 
-function toggleLight() {
-  lightOn = !lightOn;
-  document.getElementById("lightBtn").innerText = lightOn ? "ON 💡" : "OFF";
-}
+  let lightOn = false;
+  let fanOn = false;
+  let securityLocked = true;
+  let temperature = 25;
 
-function toggleFan() {
-  fanOn = !fanOn;
-  document.getElementById("fanBtn").innerText = fanOn ? "ON 🌀" : "OFF";
-}
+  window.toggleLight = function () {
+    lightOn = !lightOn;
+    document.getElementById("lightBtn").innerText = lightOn ? "ON 💡" : "OFF";
+  }
 
-function increaseTemp() {
-  temperature++;
-  document.getElementById("temp").innerText = temperature + "°C";
-}
+  window.toggleFan = function () {
+    fanOn = !fanOn;
+    document.getElementById("fanBtn").innerText = fanOn ? "ON 🌀" : "OFF";
+  }
 
-function decreaseTemp() {
-  temperature--;
-  document.getElementById("temp").innerText = temperature + "°C";
-}
+  window.increaseTemp = function () {
+    temperature++;
+    document.getElementById("temp").innerText = temperature + "°C";
+  }
 
-function toggleSecurity() {
-  securityLocked = !securityLocked;
-  document.getElementById("security").innerText =
-    securityLocked ? "Locked 🔒" : "Unlocked 🔓";
-  document.getElementById("securityBtn").innerText =
-    securityLocked ? "Unlock" : "Lock";
-}
+  window.decreaseTemp = function () {
+    temperature--;
+    document.getElementById("temp").innerText = temperature + "°C";
+  }
+
+  window.toggleSecurity = function () {
+    securityLocked = !securityLocked;
+    document.getElementById("security").innerText =
+      securityLocked ? "Locked 🔒" : "Unlocked 🔓";
+    document.getElementById("securityBtn").innerText =
+      securityLocked ? "Unlock" : "Lock";
+  }
+
+});
